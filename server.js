a4ab85658b61c4d57623b02d61ed0e027c0a1de7
Stacey Sugiono <sugiono.stacey@gmail.com>
2018-10-07 20:43:23 -0700

    added authors POST endpoint

--- server.js
+++ server.js
@@ -11,6 +11,66 @@ const { BlogPost } = require("./models");
 const app = express();
 app.use(express.json());
 
+// AUTHORS - GET
+app.get("/authors", (req, res) => {
+	Author
+		find()
+		.then(authors => {
+			res.json(authors.map(author => {
+					return {
+						id: author._id,
+						name: `${author.firstName} ${author.lastName}`,
+						userName: author.userName
+					}
+				}))
+			})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({ message: "Internal server error" });
+		})
+})
+
+//AUTHORS - POST
+app.post("/authors", (req, res) => {
+	const requiredFields = ["firstName", "lastName", "userName"];
+	for (let i = 0; i <requiredFields.length; i++){
+		const field = requiredFields[i];
+		if (!(field in req.body)){
+			const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+		}
+	}
+
+	Author 
+		.findOne({userName: req.body.userName})
+		.then(author =>{
+			if (author){
+				const message = "Username already taken";
+				console.log(message);
+				return res.status(400).send(message);
+			} else {
+				Author.create({
+					firstName: req.body.firstName,
+					lastName: req.body.lastName,
+					userName: req.body.userName
+				})
+				.then(author = res.status(201).json({
+					_id: author.id,
+					name: `${author.firstName} ${author.lastName}`,
+					userName: author.userName
+				}))
+				.catch(err => {
+					console.error(err);
+					res.status(500).json({ message: "Internal servor error" });
+				})
+			}
+		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({message: "Internal servor error"});
+		})
+})
 
 //GET request
 app.get("/posts", (req, res) => {
@@ -34,7 +94,7 @@ app.get("/posts/:id", (req, res) => {
 		.then( post => res.json(post.serialize()))
 		.cath(err => {
 			console.error(err);
-			res.status(500).json({ error: "Something went wrong" });
+			res.status(500).json({ error: "Internal server error" });
 		})
 })
 
